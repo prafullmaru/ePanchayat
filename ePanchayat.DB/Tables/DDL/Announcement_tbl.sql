84c7f90730b8143f68098a78ec679ef064473b39
Narigara007 <narigara007@gmail.com>
2024-06-19 19:30:07 +0530

    Included tables

--- ePanchayat.DB/Tables/DDL/Announcement_tbl.sql
+++ ePanchayat.DB/Tables/DDL/Announcement_tbl.sql
@@ -2,21 +2,24 @@
 
 USE [ePanchayat]
 GO
-
+ 
 SET ANSI_NULLS ON
 GO
-
+ 
 SET QUOTED_IDENTIFIER ON
 GO
-
+ 
 IF OBJECT_ID('dbo.Announcement_tbl') IS NOT NULL
 BEGIN
 	DROP TABLE dbo.Announcement_tbl
 END
-
+ 
 CREATE TABLE dbo.Announcement_tbl
 (
 	AnnouncementId INT IDENTITY(1,1) NOT NULL,
+	AnnouncementTitle VARCHAR(200) NOT NULL,
+	Description VARCHAR(1000) NOT NULL,
+	DisplayTill DATETIME NOT NULL,
 	LastModifiedOn DATETIME NOT NULL,
 	LastModifiedBy VARCHAR(50) NOT NULL,
 	IsActive BIT NOT NULL
--- ePanchayat.DB/Tables/DDL/Event_tbl.sql
+++ ePanchayat.DB/Tables/DDL/Event_tbl.sql
@@ -2,21 +2,27 @@
 
 USE [ePanchayat]
 GO
-
+ 
 SET ANSI_NULLS ON
 GO
-
+ 
 SET QUOTED_IDENTIFIER ON
 GO
-
+ 
 IF OBJECT_ID('dbo.Event_tbl') IS NOT NULL
 BEGIN
 	DROP TABLE dbo.Event_tbl
 END
-
+ 
 CREATE TABLE dbo.Event_tbl
 (
 	EventId INT IDENTITY(1,1) NOT NULL,
+	EventTitle VARCHAR(200) NOT NULL,
+	Description VARCHAR(1000) NOT NULL,
+	EventDate DATETIME NOT NULL,
+	EventTimeFrom DATETIME NOT NULL,
+	EventTimeTo DATETIME NOT NULL,
+	EventHostId INT NOT NULL,
 	LastModifiedOn DATETIME NOT NULL,
 	LastModifiedBy VARCHAR(50) NOT NULL,
 	IsActive BIT NOT NULL
