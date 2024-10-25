2df40ce350665699bad40f13bb5882b11ddad21b
Ritesh Kolhe <riteshkolhe@192.168.1.3>
2024-10-25 10:38:38 +0530

    DB Archieval Job #36 :I worked on the archival logic for Panchayat_tbl

--- ePanchayat.DB/Tables/DDL/Panchayat_tbl_history.sql
+++ ePanchayat.DB/Tables/DDL/Panchayat_tbl_history.sql
@@ -1,20 +1,13 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2761
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww37900\viewh20740\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+IF OBJECT_ID('dbo.Panchayat_tbl_history') IS NOT NULL
+BEGIN
+	DROP TABLE dbo.Panchayat_tbl_history
+END
 
-\f0\fs24 \cf0 IF OBJECT_ID('dbo.Panchayat_tbl_history') IS NOT NULL\
-BEGIN\
-	DROP TABLE dbo.Panchayat_tbl_history\
-END\
-\
-CREATE TABLE dbo.Panchayat_tbl_history\
-(\
-	PanchayatId INT IDENTITY(1,1) NOT NULL,\
-	PanchayatName VARCHAR(50) NOT NULL,\
-	LastModifiedOn DATETIME NOT NULL,\
-	LastModifiedBy INT NOT NULL,\
-	IsActive BIT NOT NULL\
-)}
+CREATE TABLE dbo.Panchayat_tbl_history
+(
+	PanchayatId INT IDENTITY(1,1) NOT NULL,
+	PanchayatName VARCHAR(50) NOT NULL,
+	LastModifiedOn DATETIME NOT NULL,
+	LastModifiedBy INT NOT NULL,
+	IsActive BIT NOT NULL
+)
